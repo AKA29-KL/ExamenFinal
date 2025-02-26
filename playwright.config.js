eeeec51f2863d99c3c22b1ef6341afb3bcfe4eba
AKA29-KL <akalaf1998@gmail.com>
2025-02-25 22:08:53 -0500

    ssssssssssssssssss

--- playwright.config.js
+++ playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   timeout: 60000,  // Augmente le timeout global pour éviter les erreurs
   use: {
-    headless: false, // 
+    headless: true, 
     viewport: { width: 1280, height: 720 },
     actionTimeout: 5000, // Timeout pour chaque action Playwright
   },
